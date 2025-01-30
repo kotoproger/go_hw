5ccd491fb02f23f13b36cf44946e80083d945d59
k.kondakov <k.kondakov@dats.team>
2025-01-30 22:47:26 +0300

    hw02

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,55 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+var (
+	ErrInvalidString = errors.New("invalid string")
+	Digits           = map[int]bool{
+		0: true,
+		1: true,
+		2: true,
+		3: true,
+		4: true,
+		5: true,
+		6: true,
+		7: true,
+		8: true,
+		9: true,
+	}
+)
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var lastSymbol string
+	var result strings.Builder
+	var escaped bool
+	for _, RuneSymbol := range input {
+		intValue, err := strconv.Atoi(string(RuneSymbol))
+
+		if err == nil && Digits[intValue] && !escaped {
+			if lastSymbol == "" {
+				return "", ErrInvalidString
+			}
+			result.WriteString(repeatRune(lastSymbol, intValue))
+			lastSymbol = ""
+		} else {
+			if !escaped && string(RuneSymbol) == "\\" {
+				escaped = true
+
+				continue
+			}
+			escaped = false
+			result.WriteString(lastSymbol)
+			lastSymbol = string(RuneSymbol)
+		}
+	}
+	result.WriteString(lastSymbol)
+
+	return result.String(), nil
+}
+
+func repeatRune(symbol string, counter int) string {
+	return strings.Repeat(symbol, counter)
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -18,11 +18,12 @@ func TestUnpack(t *testing.T) {
 		{input: "aaa0b", expected: "aab"},
 		{input: "🙃0", expected: ""},
 		{input: "aaф0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `😂5`, expected: `😂😂😂😂😂`},
+		{input: `😂5\\3`, expected: `😂😂😂😂😂\\\`},
 	}
 
 	for _, tc := range tests {
