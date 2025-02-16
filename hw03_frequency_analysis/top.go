e60a72db1b2e65909ea4d9ccd420f8a798ab2758
k.kondakov <k.kondakov@dats.team>
2025-02-16 18:27:59 +0300

    hw03

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,36 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var re = regexp.MustCompile(`^[[:punct:]]+|[[:punct:]]+$`)
+
+func Top10(input string) []string {
+	words := make(map[string]int)
+	wordsSlice := []string{}
+
+	for _, word := range strings.Fields(input) {
+		lowerCaseWord := strings.ToLower(word)
+		lowerCaseWithoutPuncts := string(re.ReplaceAll([]byte(lowerCaseWord), []byte{}))
+		if lowerCaseWithoutPuncts == "" {
+			continue
+		}
+		words[lowerCaseWithoutPuncts]++
+		if words[lowerCaseWithoutPuncts] == 1 {
+			wordsSlice = append(wordsSlice, lowerCaseWithoutPuncts)
+		}
+	}
+	if len(wordsSlice) == 0 {
+		return wordsSlice
+	}
+
+	sort.Slice(wordsSlice, func(i, j int) bool {
+		return words[wordsSlice[i]] > words[wordsSlice[j]] ||
+			(words[wordsSlice[i]] == words[wordsSlice[j]] && wordsSlice[i] < wordsSlice[j])
+	})
+
+	return wordsSlice[:10]
 }
