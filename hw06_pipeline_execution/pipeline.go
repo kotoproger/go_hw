b7e01365ccb9189135841c4a02225776990fb0f7
k.kondakov <k.kondakov@dats.team>
2025-06-05 01:14:57 +0300

    tests

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,9 @@
 package hw06pipelineexecution
 
+import (
+	"sync/atomic"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -10,29 +14,32 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	currentIn := in
+	initial := true
+	skip := atomic.Bool{}
+	go func() {
+		<-done
+		skip.Store(true)
+	}()
+
 	for _, stageItem := range stages {
-		currentIn = func(in In) (out Out) {
-			middlewareChannel := make(Bi)
-			go func() {
-				defer close(middlewareChannel)
-				for {
-					select {
-					case <-done:
-						return
-					case data, ok := <-in:
-						if !ok {
-							return
-						}
-						select {
-						case <-done:
-						case middlewareChannel <- data:
+		if initial {
+			currentIn = func(in In) (out Out) {
+				middlewareChannel := make(Bi, 1)
+				go func() {
+					defer close(middlewareChannel)
+					for data := range in {
+						if !skip.Load() {
+							middlewareChannel <- data
 						}
 					}
-				}
-			}()
+				}()
 
-			return stageItem(middlewareChannel)
-		}(currentIn)
+				return stageItem(middlewareChannel)
+			}(currentIn)
+			initial = false
+		} else {
+			currentIn = stageItem(currentIn)
+		}
 	}
 
 	return currentIn
