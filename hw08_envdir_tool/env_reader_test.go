6cb44e276181a7c94ad57eb3fd97c05c6121dfba
k.kondakov <k.kondakov@dats.team>
2026-01-09 20:30:20 +0300

    env-reader

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,83 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
 
-func TestReadDir(t *testing.T) {
-	// Place your code here
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReadDirSuccess(t *testing.T) {
+	cases := []struct {
+		name     string
+		dir      string
+		expected Environment
+	}{
+		{
+			name: "simple die",
+			dir:  "/testdata/env",
+			expected: Environment{
+				"BAR": EnvValue{
+					Value:      "bar",
+					NeedRemove: false,
+				},
+				"EMPTY": EnvValue{
+					Value:      " ",
+					NeedRemove: false,
+				},
+				"FOO": EnvValue{
+					Value:      "   foo\nwith new line",
+					NeedRemove: false,
+				},
+				"HELLO": EnvValue{
+					Value:      `"hello"`,
+					NeedRemove: false,
+				},
+				"UNSET": EnvValue{
+					Value:      ``,
+					NeedRemove: true,
+				},
+			},
+		},
+		{
+			name: "dir with subdirs",
+			dir:  "/testdata/recurse",
+			expected: Environment{
+				"EMPTY": EnvValue{
+					Value:      " ",
+					NeedRemove: false,
+				},
+				"FOO": EnvValue{
+					Value:      "   foo\nwith new line",
+					NeedRemove: false,
+				},
+				"HELLO": EnvValue{
+					Value:      `"hello"`,
+					NeedRemove: false,
+				},
+				"UNSET": EnvValue{
+					Value:      ``,
+					NeedRemove: true,
+				},
+			},
+		},
+		{
+			name:     "empty directory",
+			dir:      "/testdata/empty",
+			expected: Environment{},
+		},
+	}
+
+	for _, testCase := range cases {
+		basePath, err := os.Getwd()
+		os.Mkdir(basePath+"/testdata/empty", 0755)
+		if err != nil {
+			panic(err)
+		}
+		t.Run(testCase.name, func(t *testing.T) {
+			vars, err := ReadDir(basePath + testCase.dir)
+			assert.Nil(t, err)
+			assert.Equal(t, testCase.expected, vars)
+		})
+	}
 }
